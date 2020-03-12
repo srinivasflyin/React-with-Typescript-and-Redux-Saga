2d146112060bdef50afc785064a636ac3386bb56
srinivas s <34118095+srinivasflyin@users.noreply.github.com>
2020-03-12 18:56:13 +0530

    home page has changed

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,17 +1,15 @@
 /* eslint-disable import/first */
-import React, {Component } from 'react'
-import { FilterClass, State,Props, AppState, HotelObj } from '../../shared/classesAndInterfaces'
+import React, { Component, lazy, Suspense } from 'react'
+import { FilterClass, State, Props, AppState, HotelObj } from '../../shared/classesAndInterfaces'
 import { RouteComponentProps } from 'react-router-dom'
 import { connect } from 'react-redux';
-import Hotel from  '../../components/hotel'
-import Filters from '../../components/filters'
-//const  Hotel  = React.lazy(() => import('../../components/hotel'))
-//const Filters =  React.lazy(() => import('../../components/filters'))
+const Hotel = React.lazy(() => import('../../components/hotel'))
+const Filters = React.lazy(() => import('../../components/filters'))
 import { getFilters, getHotels } from '../../store/actions'
 
 class Home extends Component<Props>{
     public filterObj: FilterClass = { countries: [], cities: [] };
-   
+
     public hotels: Array<HotelObj> = []
     constructor(props: Props) {
         super(props)
@@ -19,7 +17,12 @@ class Home extends Component<Props>{
         this.filterObj['cities'] = []
     }
     render() {
-        return (<div><Filters  filters={this.filterObj}/> <Hotel hotels={this.hotels} /></div>)
+        return (<div>
+            <Suspense fallback={<h1>Still Loading…</h1>}>
+            <Filters filters={this.filterObj} />
+            <Hotel hotels={this.hotels} />
+            </Suspense>
+        </div>)
     }
     componentDidMount() {
         this.props.getFilters()
