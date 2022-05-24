18628d56ee14952bc9a299ab5894163774680aa6
srinivassunkari <srinivas.sunkari@valuelabs.com>
2022-05-24 21:24:07 +0530

    CH

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,34 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Home  from './pages/home/home'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home  from './pages/home/home';
+
+// merge or create new routes here
+const routes = [{
+  path: '/',
+  component: Home
+},{
+  path: '/about',
+  component: Home
+}];
+
+
+const MapRoutes = function (route: any) {
+  return (
+    <Route
+      path={route.path}
+      render={props => (
+        <route.component {...props} />
+      )}
+    />
+  );
+}
+
+
 const createRoutes = () => (<Router>
-    <Route exact path="/" component={Home} />
+  <Switch>
+  {routes.map((route, i) => (
+    <MapRoutes key={i} {...route} />
+  ))}
+</Switch>
 </Router>)
 export default createRoutes
